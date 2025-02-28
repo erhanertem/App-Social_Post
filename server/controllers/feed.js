00e3e4bbdb9c404b8c7a83ee3fd5988fe22ee512
Erhan Ertem <eertem@gmail.com>
2025-02-28 12:42:38 +0300

    Establish server-side validation that is in sync with client-side validation

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,13 +18,17 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.postPost = (req, res, next) => {
+  const errors = validationResult(req); // Extract any errors within the request object
+  // If we do have errors...
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ message: 'Validation failed, entered data is incorrect', errors: errors.array() }); // errors.array() belongs to express-validator and returns an array of reported errors
+  }
+
   const { title, content } = req.body;
 
   // Create post in db
-  res
-    .status(201)
-    .json({
-      message: 'Post created succesfully',
-      post: { _id: new Date().toISOString(), title, content, creator: { name: 'Erhan Ertem' }, createdAt: new Date() },
-    });
+  res.status(201).json({
+    message: 'Post created succesfully',
+    post: { _id: new Date().toISOString(), title, content, creator: { name: 'Erhan Ertem' }, createdAt: new Date() },
+  });
 };
--- server/routes/feed.js
+++ server/routes/feed.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator'); // Validates req.body fields
 
 const feedController = require('../controllers/feed');
 
@@ -8,6 +9,10 @@ const router = express.Router();
 router.get('/posts', feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', feedController.postPost);
+router.post(
+  '/post',
+  [body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })], // We are matching to form validatikon on the react frontend point carried out @ Feed/FeedEdit/FeedEdit.jsx
+  feedController.postPost
+);
 
 module.exports = router;
